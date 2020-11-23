a6ab616e79ec78dce9b9aec876d2da4c70077dcb
Krasimir Zahariev <k.zaharievv@protonmail.com>
2020-11-23 21:36:31 +0200

    new build and patches

--- config.def.h
+++ config.def.h
@@ -2,27 +2,38 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 24;
+static unsigned int min_lineheight = 8;
+static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+  "monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#afaf00", "#262626" },
-	[SchemeSel] = { "#262626", "#afaf00" },
-	[SchemeSelHighlight] = { "#ffc978", "#afaf00" },
-	[SchemeNormHighlight] = { "#ffc978", "#262626" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+  /*     fg         bg       */
+  [SchemeNorm] = { "#afaf00", "#262626" },
+  [SchemeSel] = { "#262626", "#afaf00" },
+  [SchemeSelHighlight] = { "#ffc978", "#afaf00" },
+  [SchemeNormHighlight] = { "#ffc978", "#262626" },
+  [SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-static unsigned int lineheight = 24;         /* -h option; minimum height of a menu line     */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 5;
+static unsigned int columns    = 2;
+
+/* history */
+static unsigned int maxhist    = 64;
+static int histnodup           = 1;	/* if 0, record repeated histories */
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
-static const char startpipe[] = "#";
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 2;
